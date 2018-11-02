eec1c5866c35012abe643a873cf18bbbae84bdd1
mattviy <mrmhamers@gmail.com>
2018-11-02 12:23:34 +0100

    project setup

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let NewQuote = function(movieTitle, director, mainActor, quoteUrl, genre) {
+    this.movieTitle = movieTitle
+    this.director = director
+    this.mainActor = mainActor
+    this.qouteUrl = qouteUrl
+    this.genre = genre
+}
+
+
+var lordOfTheRings = NewQuote (lordOfTheRings, )
